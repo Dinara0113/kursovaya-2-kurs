3cc26963e80bf0ae9f98b5cab6969eeaf93997a4
Dinara0113 <dinarkatttb16@gmail.com>
2024-11-17 12:02:04 +0500

    initial commit

---
+++ src/main/java/com/dinara/kursovaya_2_kurs/Kursovaya2KursApplication.java
@@ -0,0 +1,14 @@
+package com.dinara.kursovaya_2_kurs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Kursovaya2KursApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Kursovaya2KursApplication.class, args);
+	}
+
+
+}
